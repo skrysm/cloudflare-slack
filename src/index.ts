f20649b9b4f2e6024ece1227bca1c8710b7dd412
Sebastian Krysmanski <noreply@manski.net>
2025-12-26 14:45:27 +0100

    Cleanup files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
+ * This is a Cloudflare Workers script.
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
